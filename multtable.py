674c25e129d211fa60b9ace92408b967994723cc
hemanthtm2 <hemanthtm2@gmail.com>
2018-06-04 21:08:07 -0400

    some while loopinf

---
+++ multtable.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+def printMult(n):
+
+ i=1
+
+ while i <=10:
+   print n*i,' ',"\n"
+   i=i+1
+
+val=int(raw_input("Enter the value you want to print the multiplication table\n"))
+
+printMult(val)
